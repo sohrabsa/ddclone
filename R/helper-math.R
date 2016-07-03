028bab64cdf23e473aa472108c6a4d404e6c8f28
Sohrab Salehi <sohrab@Sohrabs-MacBook-Pro.local>
2016-07-03 03:04:12 -0700

    SS edits

--- R/helper-math.R
+++ R/helper-math.R
@@ -28,63 +28,11 @@ log.sum <- function(v) {
 }
 
 
-# harmonic mean
-# !!! what's with all these negatives?
-
-harmonic.mean <- function(x)
-{
-  -(-log(length(x)) + log.sum(-x))
-}
-
-
-# exchangeable dirichlet likelihood
-
-exch.dirichlet.lhood <- function(counts, hyper)
-{
-  k <- length(counts)
-  idx <- counts > 0
-  v <- (lgamma(k*hyper) - sum(idx)*lgamma(hyper) +
-          sum(lgamma(hyper+counts[idx])) - lgamma(sum(counts[idx])+k*hyper))
-
-
-  v
-}
-
-
-dirichlet.lhood <- function(counts, alpha)
-{
-  k <- length(counts)
-  idx <- counts > 0
-  v <- (lgamma(sum(alpha)) -
-          sum(lgamma(alpha[idx])) +
-          sum(lgamma(alpha[idx] + counts[idx])) -
-          lgamma(sum(counts)+sum(alpha)))
-
-  v
-}
-
-# draw from a dirichlet
-rdirichlet <- function(n, alpha)
-{
-  l <- length(alpha)
-  x <- matrix(rgamma(l * n, alpha), ncol = l, byrow = TRUE)
-  sm <- x %*% rep(1, l)
-  x/as.vector(sm)
-}
-
 
 # the logistic function
 logistic <- function(x) exp(x) / (1 + exp(x))
 
 
-
-
-# liu's alpha identity
-
-liu <- function(a, n)
-{
-  sum(sapply(1:n, function (i) a/(a + i - 1)))
-}
 
 
 # --- other ---
@@ -97,31 +45,5 @@ msg <- function(s, ...)
 
 char <- as.character
 
-colsum <- function (x)
-{
-  if (is.null(dim(x)) || (dim(x)[1] == 1))
-    x
-  else
-    colSums(x)
-}
 
-
-
-safelog.old <- function (x) {d
-  safelog.f <- function (x) {
-    if (x == Inf)
-      Inf
-    else if (x == 0)
-      -100
-    else
-      log(x)
-  }
-
-  if (length(x) == 1)
-    safelog.f(x)
-  else{
-    #apply(xx, seq(dim(xx)), log)
-    apply(x, 1:ifelse(is.null(dim(x)), 1, length(dim(x))), log)
-  }
-}
 
