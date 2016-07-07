fdbb6006febbb9e412913cd5dcf25ea381ebc047
Sohrab Salehi <sohrab@Sohrabs-MacBook-Pro.local>
2016-07-06 22:30:28 -0700

    Added more docs

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' my Data
+#'
+#' A dataset containing the allele counts and genotypes from a TNBC sample
+#'  The variables are as follows:
+#' \ref{Eirew P, Steif A, Khattra J, Ha G, Yap D, Farahani H, et al. Dynamics of genomic clones in breast cancer patient xenografts at single-cell resolution. Nature. 2014;.}
+#'
+#'
+"tnbc.xenograft.SA494.T"
+
+
